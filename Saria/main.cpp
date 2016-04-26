c304acd7de471b475fb958e2b3ccb5fa3473d993
Dylan Grant <dylangrant@dylans-macbook-pro.local>
2016-04-26 11:14:35 -0400

    Initial Commit

---
+++ Saria/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Saria
+//
+//  Created by Dylan Grant on 4/26/16.
+//  Copyright © 2016 Dylan Grant. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
